8a518033c03c7904baed07c82341b8cd9016aeab
Fonju-Boniface <bonifacefonju@gmail.com>
2024-10-29 14:26:20 +0100

    spit client 663

--- app/pages/locked_123_dashboard_portfolio_page_237/getcontact/page.tsx
+++ app/pages/locked_123_dashboard_portfolio_page_237/getcontact/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from "react";
 import { ref, onValue, remove, update } from "firebase/database";
 import { database } from "../../../firebase"; // Adjust the path as needed
